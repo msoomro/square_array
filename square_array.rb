59dabd6da5ac0e73deceaa18669194607900724d
Moomal <moomalmsoomro@gmail.com>
2020-11-22 17:32:04 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+
+  array.each do |num|
+    squared_array << num * num
+  end
+  
+  squared_array
 end
+
+=begin #same thing, but using .collect
+def square_array(array)
+
+  array.collect do |num|
+    num * num
+  end
+end
+=end
